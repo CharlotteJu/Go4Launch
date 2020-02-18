0dab687617991d902ce2325985de746446a330d3
Charlotte Judon <charlotte.judon@gmail.com>
2020-02-18 13:40:51 +0100

    Test pour la suppression du fichier json

--- app/src/main/java/com/example/go4lunch/view/fragments/MapViewFragment.java
+++ app/src/main/java/com/example/go4lunch/view/fragments/MapViewFragment.java
@@ -1,7 +1,6 @@
 package com.example.go4lunch.view.fragments;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -13,25 +12,8 @@ import android.view.ViewGroup;
 import com.example.go4lunch.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MapViewFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MapViewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MapViewFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public MapViewFragment() {
         // Required empty public constructor
@@ -48,20 +30,12 @@ public class MapViewFragment extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static MapViewFragment newInstance(String param1, String param2) {
         MapViewFragment fragment = new MapViewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -71,42 +45,5 @@ public class MapViewFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_map_view, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
