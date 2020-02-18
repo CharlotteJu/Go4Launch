ef4bedc4e9cfbb38132c8e8ed876765aad34d424
Charlotte Judon <charlotte.judon@gmail.com>
2020-02-18 11:58:23 +0100

    Adding activities, fragments and Firebase in Gradle

---
+++ app/src/main/java/com/example/go4lunch/view/activities/AuthActivity.java
@@ -0,0 +1,20 @@
+package com.example.go4lunch.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.go4lunch.R;
+
+/**
+ * Created by Charlotte Judon - 02/18
+ * Activity for user authentication
+ */
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
