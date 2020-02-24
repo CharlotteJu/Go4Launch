47789d9a8a32a59d15aab65a8a2c1dfa8eb31173
Charlotte Judon <charlotte.judon@gmail.com>
2020-02-24 15:03:27 +0100

    Navigation Drawer + Logout : ne fonctionne pas

--- app/src/main/java/com/example/go4lunch/view/activities/MainActivity.java
+++ app/src/main/java/com/example/go4lunch/view/activities/MainActivity.java
@@ -1,31 +1,46 @@
 package com.example.go4lunch.view.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 import com.example.go4lunch.R;
 import com.example.go4lunch.view.fragments.ListRestaurantsFragment;
 import com.example.go4lunch.view.fragments.ListWorkmatesFragment;
 import com.example.go4lunch.view.fragments.MapViewFragment;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import butterknife.BindView;
-import butterknife.BindViews;
+
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     //FOR DESIGN
     @BindView(R.id.activity_main_bottom_navigation)
     BottomNavigationView bottomNavigationView;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.navigation_drawer)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.navigation_drawer_nav_view)
+    NavigationView navigationView;
 
 
     //FOR DATA
@@ -42,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
         displayFragment(displayMapViewFragment());
         configureBottomView();
         configureToolbar();
+        configureDrawerLayout();
+        configureNavigationView();
     }
 
 
@@ -49,6 +66,19 @@ public class MainActivity extends AppCompatActivity {
     private void configureToolbar()
     {
         setSupportActionBar(toolbar);
+    }
+
+    private void configureDrawerLayout()
+    {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                                        R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void configureNavigationView()
+    {
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     //CONFIGURE VIEW
@@ -110,4 +140,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onBackPressed() {
+
+        if(this.drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id)
+        {
+            case R.id.menu_drawer_lunch :
+                break;
+            case R.id.menu_drawer_settings :
+                break;
+            case R.id.menu_drawer_logout :
+                createAndShowPopUpLogOut();
+                break;
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void createAndShowPopUpLogOut()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Êtes-vous sûr de vouloir vous déconnecter ?");
+        builder.setPositiveButton("YES", null);
+        /*builder.setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        logOut();
+                    }
+                });*/
+        builder.setNegativeButton("Non", null);
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void logOut()
+    {
+        AuthUI.getInstance().signOut(this).addOnSuccessListener(this, new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getApplicationContext(),"Déconnexion réussie",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Intent intent = new Intent(this, AuthActivity.class);
+        startActivity(intent);
+    }
 }
