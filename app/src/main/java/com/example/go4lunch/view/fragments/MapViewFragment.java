4a2edc5854800ae0d14fd57503ad2d0bddbd7506
Charlotte Judon <charlotte.judon@gmail.com>
2020-03-02 14:55:32 +0100

    Ajout des API Google MAPS et Places

--- app/src/main/java/com/example/go4lunch/view/fragments/MapViewFragment.java
+++ app/src/main/java/com/example/go4lunch/view/fragments/MapViewFragment.java
@@ -1,33 +1,43 @@
 package com.example.go4lunch.view.fragments;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.go4lunch.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
 
 
-public class MapViewFragment extends Fragment {
+public class MapViewFragment extends Fragment implements OnMapReadyCallback {
 
+
+    private GoogleMap googleMap;
 
     public MapViewFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MapViewFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MapViewFragment newInstance() {
         MapViewFragment fragment = new MapViewFragment();
         return fragment;
@@ -36,14 +46,28 @@ public class MapViewFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Places.initialize(getContext(), getString(R.string.google_api_key));
+        //PlacesClient placesClient = Places.createClient(getContext());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map_view, container, false);
+        View v = inflater.inflate(R.layout.fragment_map_view, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        return v;
     }
 
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        LatLng paris = new LatLng(48, 2.3488);
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(paris));
+        //this.googleMap.setMyLocationEnabled(true);
+
+    }
 
 }
