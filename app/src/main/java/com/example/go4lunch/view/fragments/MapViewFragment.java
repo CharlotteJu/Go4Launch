8f9016204360a4e5847e3b09bd2411dda6787f87
Charlotte Judon <charlotte.judon@gmail.com>
2020-03-04 17:39:36 +0100

    Mise en place de la localisation

--- app/src/main/java/com/example/go4lunch/view/fragments/MapViewFragment.java
+++ app/src/main/java/com/example/go4lunch/view/fragments/MapViewFragment.java
@@ -1,35 +1,40 @@
 package com.example.go4lunch.view.fragments;
 
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.LocaleList;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.go4lunch.R;
+import com.example.go4lunch.view.activities.MainActivity;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 
 public class MapViewFragment extends Fragment implements OnMapReadyCallback {
+
+    private Location currentLocation;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int REQUEST_CODE = 101;
+    private SupportMapFragment supportMapFragment;
 
 
     private GoogleMap googleMap;
@@ -48,15 +53,40 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback {
         super.onCreate(savedInstanceState);
         //Places.initialize(getContext(), getString(R.string.google_api_key));
         //PlacesClient placesClient = Places.createClient(getContext());
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+        fetchLocation();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_map_view, container, false);
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        this.supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        fetchLocation();
+        //mapFragment.getMapAsync(this);
         return v;
+    }
+
+    private void fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                    //SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+                    supportMapFragment.getMapAsync(MapViewFragment.this::onMapReady);
+                }
+            }
+        });
     }
 
 
@@ -64,10 +94,29 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
-        LatLng paris = new LatLng(48, 2.3488);
-        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(paris));
-        //this.googleMap.setMyLocationEnabled(true);
 
+        //LatLng paris = new LatLng(48, 2.3488);
+        //this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(paris));
+        //this.googleMap.setMyLocationEnabled(true)
+        // https://www.tutorialspoint.com/how-to-show-current-location-on-a-google-map-on-android*/
+
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("I am here!");
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        //this.googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        //this.googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
+        this.googleMap.addMarker(markerOptions);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchLocation();
+                }
+                break;
+        }
     }
 
 }
