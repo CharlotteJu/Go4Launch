1da6e4e288b31bc1ea8083210b3d9fcabaa463e9
Charlotte Judon <charlotte.judon@gmail.com>
2020-04-24 12:40:07 +0200

    Gitignore fichier maps xml

--- app/src/main/java/com/example/go4lunch/view/fragments/OnClickListenerItemList.java
+++
@@ -1,9 +0,0 @@
-package com.example.go4lunch.view.fragments;
-
-/**
- * Interface for the click on Restaurant List
- */
-public interface OnClickListenerItemList
-{
-    void onClickListener(int position);
-}
